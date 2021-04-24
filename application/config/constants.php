57a4517ebbe61e83624bd2fa1161d6ca7ad2ebb6
yaufaniadam1 <yaufaniadam@gmail.com>
2021-04-24 11:59:31 +0700

    constant.php

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,4 +85,4 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 //definisi file path utk generate pdf
-define('FILE_PATH' , $_SERVER['DOCUMENT_ROOT']."/layananppsjan/public/documents/");
+define('FILE_PATH' , $_SERVER['DOCUMENT_ROOT']."/public/documents/");
