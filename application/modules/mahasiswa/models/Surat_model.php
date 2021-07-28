05e8eb8c0f4488f7df4408f518caa8b9d69c9519
yaufaniadam1 <yaufaniadam@gmail.com>
2021-07-29 03:47:09 +0700

    surat baru tetap ditampilkan di admin

--- application/modules/mahasiswa/models/Surat_model.php
+++ application/modules/mahasiswa/models/Surat_model.php
@@ -30,7 +30,7 @@ class Surat_model extends CI_Model
         LEFT JOIN status st ON st.id = ss.id_status
         LEFT JOIN kategori_surat k ON k.id = s.id_kategori_surat      
         WHERE ss.id_status = (SELECT MAX(id_status) FROM surat_status WHERE id_surat=s.id)
-        AND ss.id_status != 1 $id_status
+         $id_status
         $prodi
         ORDER BY s.id DESC      
         ");
